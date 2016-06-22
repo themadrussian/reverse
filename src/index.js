20ea6045d74bd04f84ed281a7cf28fa6066ff51c
Mark Rogov <mark@ntelligen.ly>
2016-06-22 19:26:56 -0400

    working color change

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import './css/reverse.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CSSTransitionGroup from 'react-addons-css-transition-group';
 import Tappable from 'react-tappable';
-//import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 import { Button } from 'react-bootstrap';
 import { Modal } from 'react-bootstrap';
 
 import Board from './components/Board';
 import { SliderPicker } from 'react-color';
-
-
 
 const App = React.createClass({
 	getInitialState() {
@@ -36,33 +32,24 @@ const App = React.createClass({
 	},
 
 	reShape: function(rows, cols) {
-		// if(document.getElementById("rows").value != null &&
-		//	 document.getElementById("cols").value != null){
-		//}
-		console.log("Updating with: ", rows, "rows and ", cols, "cols");
+		console.log("reShape (rows x cols): ", rows, "x", cols);
 		this.setState({ rows: rows, cols: cols, update: 1});
-		//this.state.cols = cols;
-		//this.state.update = 1;
-
-		console.log("rows:", this.state.rows, "cols:", this.state.cols);
 	},
 
-	handleColorchange: function(which, color) {
-		console.log("selected color:", color.hex);
-
+	handleColorChange: function(which, color) {
+		this.setState({update: 0}); //reset this
 		if (which === "colorFrom") {
 			this.setState({ colorA: color.hex, update: 0});
-			console.log("and change ColorA to:", this.state.colorA)
-		}
-		if (which === "colorTo") {
+			console.log("Change ColorA to:", this.state.colorA)
+		} else if (which === "colorTo") {
 			this.setState({ colorB: color.hex, update: 0});
 			console.log("and change ColorB to:", this.state.colorB)
+		} else {
+			console.log("handleColorChange: recieved incorrect 'which': ", which);
 		}
-
 	},
 
 	render() {
-    const { animationName } = this.state;
 		let close = () => this.setState({ showModalA: false, showModalB: false});
 
 		return (
@@ -76,18 +63,16 @@ const App = React.createClass({
 	        <Button
 	          bsStyle="primary"
 	          bsSize="large"
-	          onClick={() => this.setState({ showModalA: true, showModalB: false})}
-						style={{ backgroundColor: this.state.colorA, width: "80px"}}
-	        >
+	          onClick={() => this.setState({ showModalA: true, showModalB: false, update: 0})}
+						style={{ backgroundColor: this.state.colorA, width: "80px"}}>
 	          From
 	        </Button>
 					&nbsp;&nbsp;
 					<Button
 	          bsStyle="primary"
 	          bsSize="large"
-	          onClick={() => this.setState({ showModalB: true, showModalA: false})}
-						style={{ backgroundColor: this.state.colorB, width: "80px"}}
-	        >
+	          onClick={() => this.setState({ showModalB: true, showModalA: false, update: 0})}
+						style={{ backgroundColor: this.state.colorB, width: "80px"}}>
 	          To
 	        </Button>
 
@@ -95,44 +80,32 @@ const App = React.createClass({
 	          show={this.state.showModalA}
 	          onHide={close}
 	          container={this}
-	          aria-labelledby="contained-modal-title"
-	        >
-	          <Modal.Header closeButton>
-	            <Modal.Title id="contained-modal-title">From Color</Modal.Title>
-	          </Modal.Header>
+	          aria-labelledby="contained-modal-title">
 	          <Modal.Body>
-								<SliderPicker onChange={this.handleColorchange.bind(this,"colorFrom")} color={this.state.colorA}/>
+								<SliderPicker onChange={this.handleColorChange.bind(this,"colorFrom")} color={this.state.colorA}/>
 	          </Modal.Body>
 	        </Modal>
+
 					<Modal
 	          show={this.state.showModalB}
 	          onHide={close}
 	          container={this}
-	          aria-labelledby="contained-modal-title"
-	        >
-	          <Modal.Header closeButton>
-	            <Modal.Title id="contained-modal-title">To Color</Modal.Title>
-	          </Modal.Header>
+	          aria-labelledby="contained-modal-title">
 	          <Modal.Body>
-								<SliderPicker onChange={this.handleColorchange.bind(this,"colorTo")} color={this.state.colorB}/>
+								<SliderPicker onChange={this.handleColorChange.bind(this,"colorTo")} color={this.state.colorB}/>
 	          </Modal.Body>
 	        </Modal>
 
 	      </div>
 				<div className="board">
-	        <CSSTransitionGroup transitionName={ animationName }
-	            transitionEnterTimeout={ 300 } transitionLeaveTimeout={ 300 }>
-							<Board
-								rows={this.state.rows}
-								cols={this.state.cols}
-								update={this.state.update}
-								colorA={this.state.colorA}
-								colorB={this.state.colorB}
-								/>
-	        </CSSTransitionGroup>
+					<Board
+						rows={this.state.rows}
+						cols={this.state.cols}
+						update={this.state.update}
+						colorA={this.state.colorA}
+						colorB={this.state.colorB}
+					/>
 				</div>
-
-
 			</div>
 		);
 	},
